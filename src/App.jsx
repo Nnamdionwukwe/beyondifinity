f482b25557e78f00f2b7c5088b6452162026884e
Onwukwennamdi <nnamdionwukwe@gmail.com>
2026-06-30 03:41:09 +0100

    dsfds

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
 
 function App() {
-  return <LandingPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        {/* Add more routes here as your site grows */}
+        {/* <Route path="/products" element={<ProductsPage />} /> */}
+        {/* <Route path="/about" element={<AboutPage />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
 export default App;
